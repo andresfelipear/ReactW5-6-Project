07cbb62f006a0c93b4ef58b3ba76fd10d8fc956a
Francois Polo <andasan@gmail.com>
2021-12-03 13:50:12 -0800

    added collections page and overview

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from 'react'
 import { useSelector } from 'react-redux'
 
 // import './directory.styles.scss';
 import { DirectoryMenuContainer } from './directory.styles'
 
-import MenuItem from '../menu-item/menu-item.component';
+import MenuItem from '../menu-item/menu-item.component'
+
 const Directory = () => {
-  const directory = useSelector(state => state.directory)
-  console.log(directory);
-    return (
-      <DirectoryMenuContainer>
-        {directory.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} className={title}  />
-        ))}
-      </DirectoryMenuContainer>
-    );
+  const directory = useSelector((state) => state.directory)
+  
+  return (
+    <DirectoryMenuContainer>
+      {directory.sections.map(({ title, imageUrl, id, size }) => (
+        <MenuItem
+          key={id}
+          title={title}
+          imageUrl={imageUrl}
+          size={size}
+          className={title}
+        />
+      ))}
+    </DirectoryMenuContainer>
+  )
 }
 
-export default Directory;
+export default Directory
