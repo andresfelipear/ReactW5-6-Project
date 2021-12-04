2c1eeef9de7cbc612a55f7291d388d3fcb17bae6
Francois Polo <andasan@gmail.com>
2021-12-03 16:11:52 -0800

    add to cart feature implemented

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
 // import './cart-dropdown.styles.scss';
 import {
@@ -10,15 +11,19 @@ import {
 import CartItem from '../cart-item/cart-item.component';
 
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = () => {
+  
+  const cartItems = useSelector(state => state.cart.cartItems)
+
+  return(
   <CartDropdownContainer>
-    {/* <CartItemsContainer>
+    <CartItemsContainer>
       {cartItems.map(cartItem => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
-    </CartItemsContainer> */}
+    </CartItemsContainer>
     <CartDropdownButton>GO TO CHECKOUT</CartDropdownButton>
   </CartDropdownContainer>
-);
+)};
 
 export default CartDropdown;
--- src/pages/collection/collection-page.component.jsx
+++ src/pages/collection/collection-page.component.jsx
@@ -6,17 +6,19 @@ import {
   CollectionTitle,
 } from './collection-page.styles'
 
+import CollectionItem from 'components/collection-item/collection-item.component'
+
 const CollectionPage = ({ collections }) => {
+  const { title, items } = collections
 
-    const { title, items } = collections
-
+  console.log('CP: ', collections);
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
       <CollectionItemsContainer>
-      {items.map(item => (
-          <>{item.id}</>
-      ))}
+        {items && items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
       </CollectionItemsContainer>
     </CollectionPageContainer>
   )
