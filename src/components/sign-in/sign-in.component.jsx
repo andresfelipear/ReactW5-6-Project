ed175a68902b1882ce2567b1d505ff726b725470
Francois Polo <andasan@gmail.com>
2021-12-06 15:12:21 -0800

    firebase refactor to v9

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -9,6 +9,8 @@ import {
 } from './sign-in.styles'
 
 import CustomButton from 'components/custom-button/custom-button.component'
+import FormInput from 'components/form-input/form-input.component'
+import { auth, signInWithGoogle } from 'firebase/firebase.util'
 
 const SignIn = () => {
   const [user, setUser] = useState({
@@ -19,35 +21,51 @@ const SignIn = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
 
+    console.log(user)
     // handle with firebase
   }
 
   const handleChange = (event) => {
     const { value, name } = event.target
 
-    setUser(prevState => ({
-        ...prevState,
-        [name]: value
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value,
     }))
   }
 
-  return(
-      <SignInContainer>
-          <SignInTitle>I already have an account</SignInTitle>
-          <span>Sign in with your email and password</span>
+  return (
+    <SignInContainer>
+      <SignInTitle>I already have an account</SignInTitle>
+      <span>Sign in with your email and password</span>
 
-          <form onSumit={handleSubmit}>
-              {/* email */}
-              {/* password */}
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          handleChange={handleChange}
+          name='email'
+          type='email'
+          label='Email'
+          value={user.email || ''}
+          required
+        />
+        <FormInput
+          handleChange={handleChange}
+          name='password'
+          type='password'
+          label='Password'
+          value={user.password || ''}
+          required
+        />
 
-              <ButtonsBarContainer>
-                  <CustomButton type="submit">Sign In</CustomButton>
-                  {/* <CustomButton type="button" onClick={signInWithGoogle}>Sign in with Google</CustomButton> */}
-              </ButtonsBarContainer>
-          </form>
-      </SignInContainer>
+        <ButtonsBarContainer>
+          <CustomButton type='submit'>Sign In</CustomButton>
+          <CustomButton type='button' onClick={signInWithGoogle}>
+            Sign in with Google
+          </CustomButton>
+        </ButtonsBarContainer>
+      </form>
+    </SignInContainer>
   )
 }
-
 
 export default SignIn
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import SignIn from 'components/sign-in/sign-in.component'
+import SignUp from 'components/sign-up/sign-up.component'
 
-import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles'
+import {SignInAndSignUpContainer} from './sign-in-and-sign-up.styles'
 
 const SignInAndSignUp = () => (
     <SignInAndSignUpContainer>
-        {/* sign in component */}
-        {/* sign up component */}
+        <SignIn />
+        <SignUp />
     </SignInAndSignUpContainer>
 )
 
